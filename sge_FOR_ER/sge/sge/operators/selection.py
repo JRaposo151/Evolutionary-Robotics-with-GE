e45d5b441739f5d1afa91570eea872aac65f82f4
JRaposo151 <jeraposo151@gmail.com>
2025-07-30 02:35:46 +0100

    Update on crossover

--- sge_FOR_ER/sge/sge/operators/selection.py
+++ sge_FOR_ER/sge/sge/operators/selection.py
@@ -4,5 +4,5 @@ import copy
 
 def tournament(population, tsize=3):
     pool = random.sample(population, tsize)
-    pool.sort(key=lambda i: i['fitness'])
+    pool.sort(key=lambda i: i['fitness'], reverse=True)
     return copy.deepcopy(pool[0])
