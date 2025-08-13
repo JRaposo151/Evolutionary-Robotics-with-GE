32c918c85d888b5504e914fc8183960de0d0c911
JRaposo151 <jeraposo151@gmail.com>
2025-08-13 21:46:38 +0100

    Update

--- sge_FOR_ER/sge/sge/engine.py
+++ sge_FOR_ER/sge/sge/engine.py
@@ -100,7 +100,7 @@ def evolutionary_algorithm(evaluation_function=None, parameters_file=None):
     else:
         print("Directory is not empty")
         # === 1. Load saved population from previous generation ===
-        checkpoint_path = directory + "/run_1/iteration_6.json"
+        checkpoint_path = directory + "/run_1/iteration_10.json"
         with open(checkpoint_path, "r") as f:
             population = json.load(f)
 
